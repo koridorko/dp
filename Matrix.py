a7b4ae3b8e04fc5c46b0d3860b3dedc15093dc16
stevikg <stevikg@gmail.com>
2025-10-01 15:07:12 +0200

    some matrix related utility methods

--- Matrix.py
+++ Matrix.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import random
 
 class MatrixEventType(Enum):
     INVITE = "m.call.invite"
@@ -14,3 +15,94 @@ class MatrixMessage():
         # but to keep it similar to SIPMessage class, we will use message_type
         self.message_type: MatrixEventType | None = None
 
+
+
+class MatrixPayloadCreator():
+    @staticmethod
+    def create_invite_payload(sdp: str, call_id: str, version: int, lifetime: int = 60000) -> dict:
+        """Create payload for m.call.invite event"""
+        payload = {
+            "content": {
+                "body": "SIP to Matrix call",
+                "call_id": call_id,
+                "version": version,
+                "lifetime": lifetime,
+                "offer": {
+                    "type": "offer",
+                    "sdp": sdp
+                }
+            },
+            "event_type": MatrixEventType.INVITE.value,
+            "txn_id": "12345"  # This should be unique for each transaction
+        }
+        return payload
+
+    @staticmethod
+    def create_answer_payload(sdp: str, call_id: str, version: int) -> dict:
+        """Create payload for m.call.answer event"""
+        payload = {
+            "content": {
+                "call_id": call_id,
+                "version": version,
+                "answer": {
+                    "type": "answer",
+                    "sdp": sdp
+                }
+            },
+            "event_type": MatrixEventType.ANSWER.value,
+            "txn_id": "12346"  # This should be unique for each transaction
+        }
+        return payload
+
+    @staticmethod
+    def create_hangup_payload(call_id: str, version: int) -> dict:
+        """Create payload for m.call.hangup event"""
+        payload = {
+            "content": {
+                "call_id": call_id,
+                "version": version
+            },
+            "event_type": MatrixEventType.HANGUP.value,
+            "txn_id": "12347"  # This should be unique for each transaction
+        }
+        return payload
+
+    @staticmethod
+    def create_candidate_payload(call_id: str, 
+                                 version: int, 
+                                 sdp_mid: str, 
+                                 sdp_mline_index: int) -> dict:
+        """Create payload for m.call.candidate event"""
+        payload = {
+            "content": {
+                "call_id": call_id,
+                "version": version,
+            },
+            "candidate":
+                {
+                    "candidate": f"candidate:1 1 UDP 2130706431"
+                },
+            "sdpMid": sdp_mid,
+            "sdpMLineIndex": sdp_mline_index,
+            "event_type": MatrixEventType.CANDIDATE.value,
+            "txn_id": str(random.randint()) # This should be unique for each transaction
+        }
+
+        return payload
+    
+    @staticmethod
+    def create_select_answer_payload(call_id: str, version: int, sdp: str) -> dict:
+        """Create payload for m.call.select_answer event"""
+        payload = {
+            "content": {
+                "call_id": call_id,
+                "version": version,
+                "answer": {
+                    "type": "answer",
+                    "sdp": sdp
+                }
+            },
+            "event_type": MatrixEventType.SELECT_ANSWER.value,
+            "txn_id": str(random.randint())  # This should be unique for each transaction
+        }
+        return payload
