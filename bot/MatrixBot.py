22e662b3f7f11edbf53002c1573dd6c15e98fba6
stevikg <stevikg@gmail.com>
2025-10-02 17:16:55 +0200

    Some cahnges in bot class

--- bot/MatrixBot.py
+++ bot/MatrixBot.py
@@ -1,14 +1,21 @@
 """File with implementation of Matrix Bot Class"""
+
 import os
-from nio import AsyncClient, SyncResponse, RoomMessageText 
+
+
+from nio import AsyncClient
+
 
 class MatrixBotException(Exception):
     """Base class for Matrix Bot exceptions"""
+
     pass
+
 
 class MatrixBot:
     """Class representing a Matrix bot"""
-    def __init__(self)-> None:
+
+    def __init__(self) -> None:
         # initializing bot with env variables
         self.username = os.environ.get("MATRIX_BOT_USERNAME", None)
         self.password = os.environ.get("MATRIX_BOT_PASSWORD", None)
@@ -17,34 +24,41 @@ class MatrixBot:
         self.user_id = os.environ.get("MATRIX_BOT_USER_ID", None)
         self.client: AsyncClient | None = None
 
-        if not all([self.username, 
-                    self.password,
-                    self.homeserver, 
-                    self.access_token, 
-                    self.user_id]):
-            raise MatrixBotException("Missing one or more required environment variables for Matrix Bot")
+        if not all(
+            [
+                self.username,
+                self.password,
+                self.homeserver,
+                self.access_token,
+                self.user_id,
+            ]
+        ):
+            raise MatrixBotException(
+                "Missing one or more required environment variables for Matrix Bot"
+            )
 
-        try:
-            self.connect_to_server()
-        except Exception as e:
-            raise MatrixBotException(f"Failed to connect to Matrix server: {e}") 
+        # try:
+        #     self.connect_to_server()
+        # except Exception as e:
+        #     raise MatrixBotException(f"Failed to connect to Matrix server: {e}")
 
-    def connect_to_server(self) -> None:
-        """Method to connect to the Matrix server"""
-        # self client will be latter used for sending and receiving messages
-        self.client = AsyncClient(self.homeserver, self.user_id)
-        self.client.access_token = self.access_token
-        response = self.client.sync()
-        if not isinstance(response, SyncResponse):
-            raise MatrixBotException("Failed to sync with the Matrix server")
-        
-        print("Connected to Matrix server successfully")
+    # async def connect_to_server(self) -> None:
+    #     """method to connect to the matrix server"""
+    #     # self client will be latter used for sending and receiving messages
+    #     self.client = AsyncClient(self.homeserver, self.user_id)
+    #     self.client.access_token = self.access_token
+    #     response = await self.client.sync()
+    #     if not isinstance(response, SyncResponse):
+    #         raise MatrixBotException("Failed to sync with the Matrix server")
+    #
+    #     print("Connected to Matrix server successfully")
 
 
 def test_bot_initialization():
     """Test function to check bot initialization"""
     try:
         bot = MatrixBot()
+        print(bot)
         print("Matrix Bot initialized successfully")
     except MatrixBotException as e:
         print(f"Matrix Bot initialization failed: {e}")
