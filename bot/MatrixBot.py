74d859849386c54cb63c37d81e6db62aa0a5a739
stevikg <stevikg@gmail.com>
2025-10-03 11:11:04 +0200

    Test bot initialization and returning bot object

--- bot/MatrixBot.py
+++ bot/MatrixBot.py
@@ -1,7 +1,7 @@
 """File with implementation of Matrix Bot Class"""
 
 import os
-
+import sys
 
 from nio import AsyncClient, SyncResponse
 
@@ -17,7 +17,7 @@ class MatrixBot:
 
     def __init__(self) -> None:
         # initializing bot with env variables
-        # HACK:
+        # NOTE:
         # using empty string to ensure it is not None type and don't have to
         # duplicate all the checks after __init__ function
         self.username = os.environ.get("MATRIX_BOT_USERNAME", "")
@@ -40,33 +40,38 @@ class MatrixBot:
                 "Missing one or more required environment variables for Matrix Bot"
             )
 
-        # try:
-        #     self.connect_to_server()
-        # except Exception as e:
-        #     raise MatrixBotException(f"Failed to connect to Matrix server: {e}")
-
-    async def connect_to_server(self) -> None:
+    async def connect_to_server(self) -> bool:
         """method to connect to the matrix server"""
         # self client will be latter used for sending and receiving messages
-
         self.client = AsyncClient(self.homeserver, self.user_id)
         self.client.access_token = self.access_token
         response = await self.client.sync()
         if not isinstance(response, SyncResponse):
-            raise MatrixBotException("Failed to sync with the Matrix server")
+            return False
+        print("Connected to Matrix server successfully", file=sys.stderr)
+        return True
 
-        print("Connected to Matrix server successfully")
 
-
-def test_bot_initialization():
+def initialize_bot() -> MatrixBot:
     """Test function to check bot initialization"""
-    try:
-        bot = MatrixBot()
-        print(bot)
-        print("Matrix Bot initialized successfully")
-    except MatrixBotException as e:
-        print(f"Matrix Bot initialization failed: {e}")
+    bot = MatrixBot()
+    if not bot.connect_to_server():
+        raise MatrixBotException(
+            "Could not connect to server specified in MATRIX_BOT_HOMESERVER env variable!"
+        )
+    return bot
+
+
+# NOTE: usage bude cca nieco ako:
+#       clovek si zapne (bude to v kontajneroch)
+#       SIP server a Matrix bota
+#       (oba si bude musiet "nakonfigurovat")
+#       teda:
+#           pre bota si bude musiet zohnat credentials a upravit env
+#           toho bota bude vyuzivat tak ze ked bude chciet volat SIP -> Matrix
+#       a tiez bude mat  nakonfigurovany sip server, ktory bude pouzity na volanie Matrix -> SIP
+#       a bude to vlastne fungovat tak ze matrix acc -> matrix bot -> SIP server -> Iny sip user
 
 
 if __name__ == "__main__":
-    test_bot_initialization()
+    bot = initialize_bot()
